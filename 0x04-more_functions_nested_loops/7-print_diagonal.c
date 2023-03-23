e5dcdf4c26dc8b1fc09004a0ecf9433b94eabf44
Eslam Ali <eslamalimuhamed@gmail.com>
2023-03-23 03:30:13 -0700

    fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,8 +18,8 @@ void print_diagonal(int n)
 		for (postn = 1; postn <= n; ++postn)
 		{
 			for (space = 1; space <= postn; ++space)
-			_putchar(' ');
-			_putchar(92);
+				_putchar(' ');
+			_putchar('/');
 			_putchar('\n');
 		}
 	}
