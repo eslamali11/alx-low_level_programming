2e45a3d7cfa758a72fba3f4d9853c87be1dfb8ea
Eslam Ali <eslamalimuhamed@gmail.com>
2023-04-05 05:54:44 -0700

    fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(char *s + 1);
 	}
 
 	else
-		_putchar('\n);
+		_putchar('\n');
 
 }
