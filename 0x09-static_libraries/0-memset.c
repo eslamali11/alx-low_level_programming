c3d813b48724a1b2dbea754f8b0e0f36baaf0f4b
Eslam Ali <eslamalimuhamed@gmail.com>
2023-04-06 12:43:46 -0700

    done

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset -  function that fills memory with a constant byte.
+ * @s: constant byte
+ * @b: area
+ * @n: bytes of memory
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+		s[i] = b;
+
+	return (s);
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches for bytes
+ *
+ * @s: the string
+ *
+ * @accept: found the bytes, or @NULL if no byte
+ *
+ * Return: a pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int indx;
+
+		while (*s)
+		{
+			for (indx = 0; accept[indx]; indx++)
+			{
+				if (*s == accept[indx])
+					return (s);
+			}
+			s++;
+		}
+	return ('\0');
+}
