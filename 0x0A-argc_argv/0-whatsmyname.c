187142b742477a8f715fb68e84fa7b75ef6dd1de
Eslam Ali <eslamalimuhamed@gmail.com>
2023-04-07 14:37:06 -0700

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ *
+ * @argc: holds the numper of argument
+ * @argv: array of arguments
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
