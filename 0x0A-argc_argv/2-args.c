3684ce933891df1beeebb09c0352ed2d76b673f8
Eslam Ali <eslamalimuhamed@gmail.com>
2023-04-07 15:48:06 -0700

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print all arguments with new line for each
+ *
+ * @argc: count
+ * @argv: array
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
