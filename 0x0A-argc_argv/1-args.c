1f38aa0204d4494c18b943a6dde2b905f771971e
Eslam Ali <eslamalimuhamed@gmail.com>
2023-04-07 15:14:29 -0700

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the numper of arguments
+ *
+ * @argc: the numper of argument
+ * @argv: array of argument
+ *
+ * Return: 0
+*/
+
+int main(int argc; char **argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	
+	return (0);
+}
