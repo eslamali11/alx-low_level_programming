69fc13159860621016d8f201acca76babfcaf397
Eslam Ali <eslamalimuhamed@gmail.com>
2023-04-08 07:44:27 -0700

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplie tow num
+ *
+ * @argc: counter
+ * @argv: array
+ *
+ * Return: 1 if Error
+*/
+
+int main(int argc, char *argv[])
+{
+	int mul = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+
+	return (0);
+}
