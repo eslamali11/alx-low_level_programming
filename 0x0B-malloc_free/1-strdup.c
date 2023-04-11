33429f5a5004dff4bd93c151261966a2aef27fd5
Eslam Ali <eslamalimuhamed@gmail.com>
2023-04-11 12:51:56 -0700

    done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib>
+
+/**
+ * _strdup - return a pointer to a copy of string 
+ *
+ * @str: the given parameter
+ *
+ * Return: NULL if str is NULL
+ *
+*/
+
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	s = malloc((len + 1) * 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		s[i] = str[i];
+		i++;
+	}
+	return (s);
