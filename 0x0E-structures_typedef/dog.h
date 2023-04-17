d869e75febb5dd04799b7cfc7e9afa886ba15636
Eslam Ali <eslamalimuhamed@gmail.com>
2023-04-17 12:29:46 -0700

    dog 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define type
+ *
+ * @name: the name
+ * @age: the age
+ * @owner: the owner name
+ *
+ *
+*/
+
+struct dog {
+	char* name;
+	float age;
+	char* owner;
+};
+
+#endif
