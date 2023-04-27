a99b4f531006b4a175f16cb0136a157ed41589dd
eslamali11 <eslamalimuhamed@gmail.com>
2023-04-27 14:56:48 -0700

    fix betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 
-/*
- * print_list - print all elements in a linked list
+/**
+ * print_list - Prints the elements of a linked list
  *
- * @h: the head
- * 
- * Return: numper of nodes
+ * @h: The Head
+ *
+ * Return: number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h)
 	{
 		if (h->str == NULL)
