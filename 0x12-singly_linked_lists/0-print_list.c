b20e3e352fe23e2432e8e0bf7931f6f48cc8efa7
eslamali11 <eslamalimuhamed@gmail.com>
2023-04-27 14:38:27 -0700

    for readme

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/*
+ * print_list - print all elements in a linked list
+ *
+ * @h: the head
+ * 
+ * Return: numper of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
