cebe85cb3a29ecc4280aad32fb1f01de3e0bbf59
Eslam Ali <eslamalimuahmed@gmail.com>
2023-04-29 18:18:46 +0000

    fix syntax

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,8 +13,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode;
 
-	newNode = (listint_t *)malloc(sizeof(listint_t);
-
+	newNode = (listint_t *)malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
 
